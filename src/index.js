a1b35de5ebf08ee0f2f4b51e297c1ce194e11396
Vladislav <taranov.vlad.1995@gmail.com>
2021-09-03 10:30:24 +0300

    Realized accordion

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import './css/style.scss'
+
+const btnArr = document.querySelectorAll('.accordion-item__header')
+const contentTextArr = document.querySelectorAll('.accordion-item__content')
+const slider = document.querySelector('.switch-btn')
+
+btnArr.forEach(item => {
+    item.addEventListener('click', function () {
+
+        let contentText = this.nextElementSibling;
+        contentText.classList.toggle('active');
+
+        if(slider.classList.contains('switch-on')) {
+            closeOtherAccordion(contentText)
+        }
+    })
+})
+
+slider.addEventListener('click', function () {
+    this.classList.toggle('switch-on');
+});
+
+function closeOtherAccordion(item) {
+    for(let i = 0; i < contentTextArr.length; i++) {
+        if(contentTextArr[i] !== item) {
+            contentTextArr[i].classList.remove('active');
+        }
+    }
+}
